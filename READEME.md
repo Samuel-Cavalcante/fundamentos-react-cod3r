8d30563ffb7f547bfead410aea27cbc62f4352d3
xandsc <samuelscav@hotmail.com>
2022-07-05 18:27:13 -0300

    Aula 01

---
+++ READEME.md
@@ -0,0 +1,13 @@
+Link das aulas do canal Cod3r Cursos
+
+
+
+React JS Curso Rápido ⚛️ Masterclass #1 2020 - 100% Prático!
+
+watch?v=XQxitgyZ_S4&t=1453s&ab_channel=Cod3rCursos
+
+------------------------------------------------------------
+
+React JS Curso Rápido ⚛️ Masterclass #2 2020 - 100% Prático!
+
+watch?v=GJ8Vm-h0V8I&ab_channel=Cod3rCursos
---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './App.css'
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+import ComFilhos from './components/basicos/ComFilhos'
+import Card from './components/layout/Card'
+import Repeticao from './components/basicos/Repeticao'
+import Condicional from './components/basicos/Condicional'
+import CondicionalComIf from './components/basicos/CondicionalComIf'
+
+export default (props) => (
+    <div className="App">
+
+        <Card titulo='#06 - Condicional v2'>
+            <CondicionalComIf numero={10}></CondicionalComIf>
+        </Card>
+
+        <Card titulo='#05 - Condicional v1'>
+            <Condicional numero={10}></Condicional>
+        </Card>
+
+        <Card titulo='#04 - Repeticao'>
+            <Repeticao></Repeticao>
+        </Card>
+
+        <Card titulo='#03 - Componente com Filhos'>
+            <ComFilhos>
+                <ul>
+                    <li>Ana</li>
+                    <li>Bia</li>
+                    <li>Carlos</li>
+                    <li>Samuel</li>
+                </ul>
+            </ComFilhos> 
+        </Card>
+
+        <Card titulo='#02 - Componente com Parametro'>
+            <ComParametro titulo="Esse é o título"
+                subtitulo="Esse é o subtitulo" />
+        </Card>
+
+        <Card titulo='#01 - Primeiro Componente'>
+            <Primeiro />
+        </Card>
+
+    </div>
+);
